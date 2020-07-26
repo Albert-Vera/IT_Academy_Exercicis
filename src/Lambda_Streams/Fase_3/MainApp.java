40a40b40bd81b804cc8f5e927b0552157264983b
albert <albert.vedu@gmail.com>
2020-07-26 10:55:57 +0200

    commit

--- src/Lambda_Streams/Fase_3/MainApp.java
+++ src/Lambda_Streams/Fase_3/MainApp.java
@@ -3,13 +3,13 @@ package Lambda_Streams.Fase_3;
 public class MainApp {
     public static void main(String[] args) {
 
-        String_Reverse string_reverse = (string) -> {
+        String_Reverse string_reverse = string -> {
             string.chars()
                     .mapToObj(e -> String.valueOf((char)e)) //convierte cada letra a String
                     .reduce((a, e)-> e.concat(a)) //concadena, pero con el orden contrario
                     .ifPresent(System.out::println);
             return string ;
         };
-        string_reverse.reverse("madrid");
+        string_reverse.reverse("Bon dia Barcelona");
     }
 }
